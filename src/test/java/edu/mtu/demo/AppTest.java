c69a733c6d7a5c46e8868791b905bac1af29405a
SereinR <98984783+SereinR@users.noreply.github.com>
2025-11-12 15:12:59 -0500

    Create AppTest.java

---
+++ src/test/java/edu/mtu/demo/AppTest.java
@@ -0,0 +1,11 @@
+package edu.mtu.demo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    void alwaysPasses() {
+        assertTrue(true);
+    }
+}
